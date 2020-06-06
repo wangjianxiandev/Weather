26b9179785811b8e56a581bcd88ed62c03b55eaa
wangjianxiandev <jianxianwang198@163.com>
2020-06-06 18:02:49 +0800

    完善首页界面

--- app/src/main/java/com/wjx/android/weather/module/home/HomeFragment.kt
+++ app/src/main/java/com/wjx/android/weather/module/home/HomeFragment.kt
@@ -5,18 +5,29 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.wjx.android.weather.R
 import com.wjx.android.weather.base.view.BaseLifeCycleFragment
 import com.wjx.android.weather.common.Utils
 import com.wjx.android.weather.databinding.HomeFragmentBinding
+import com.wjx.android.weather.module.home.adapter.HourAdapter
 import kotlinx.android.synthetic.main.home_fragment.*
 import kotlinx.android.synthetic.main.home_fragment.view.*
+import kotlinx.android.synthetic.main.layout_container.*
 
 class HomeFragment : BaseLifeCycleFragment<HomeViewModel, HomeFragmentBinding>() {
+
+    private lateinit var mAdapter: HourAdapter
+
+    private val mPlaceList = arrayListOf(
+        "a", "c", "b", "d", "f"
+    )
+
     override fun initView() {
         super.initView()
         setHasOptionsMenu(true)
         initToolbar()
+        initAdapter()
     }
 
     override fun getLayoutId() = R.layout.home_fragment
@@ -26,9 +37,10 @@ class HomeFragment : BaseLifeCycleFragment<HomeViewModel, HomeFragmentBinding>()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
+        when (item.itemId) {
             R.id.action_city -> {
-                Navigation.findNavController(home_container).navigate(R.id.action_homeFragment_to_choosePlaceFragment)
+                Navigation.findNavController(home_container)
+                    .navigate(R.id.action_homeFragment_to_choosePlaceFragment)
 //                Utils.showToast(requireContext(), getString(R.string.city))
             }
             R.id.action_more -> {
@@ -42,5 +54,12 @@ class HomeFragment : BaseLifeCycleFragment<HomeViewModel, HomeFragmentBinding>()
         home_bar.home_title.text = "北京"
         home_bar.setTitle("")
         (requireActivity() as AppCompatActivity).setSupportActionBar(home_bar)
+    }
+
+    private fun initAdapter() {
+        mAdapter = HourAdapter(R.layout.hourly_item, mPlaceList)
+        home_recycler.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        home_recycler.adapter = mAdapter
     }
 }
